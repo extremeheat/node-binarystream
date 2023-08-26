839d81688a2cec0f2d295f4e0c393082e1ef1e45
extremeheat <extreme@protonmail.ch>
2023-08-25 23:52:57 -0400

    Update example

--- example.js
+++ example.js
@@ -3,6 +3,3 @@ const stream = new ByteStream()
 stream.writeStringNT('hello world!')
 
 console.log('Encoded hello world: ', stream.getBuffer())
-
-const stream2 = new ByteStream()
-stream2.writeStringNT('hello world!')
