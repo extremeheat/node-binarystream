1d3aad8f053e466bdeb7d0c8085e789c3d62362d
extremeheat <extreme@protonmail.ch>
2023-08-29 17:17:29 -0400

    Fix bug writing 64bit integers with JS Number's (#1)

--- test/basic.test.js
+++ test/basic.test.js
@@ -8,4 +8,14 @@ describe('basic tests', () => {
     stream.writeStringNT('hello world!')
     assert(stream.getBuffer().equals(Buffer.from('hello world!\0')))
   })
+  it('Numbers with i64', () => {
+    const stream = new ByteStream()
+    stream.writeInt64LE(1)
+    assert(stream.getBuffer().equals(Buffer.from([1, 0, 0, 0, 0, 0, 0, 0])))
+  })
+  it('Negative Numbers with i64', () => {
+    const stream = new ByteStream()
+    stream.writeInt64LE(-1)
+    assert(stream.getBuffer().equals(Buffer.from([255, 255, 255, 255, 255, 255, 255, 255])))
+  })
 })
